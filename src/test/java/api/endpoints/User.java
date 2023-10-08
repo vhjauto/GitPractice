dd56ddf469a37e38cae0e0fd11237b35892ca79c
vhjadhav9096@gmail.com <vhjadhav9096@gmail.com>
2023-10-08 12:28:03 +0530

    first commit

---
+++ src/test/java/api/endpoints/User.java
@@ -0,0 +1,29 @@
+package api.endpoints;
+
+public class User {
+	
+	
+	Integer id;
+	String first_Name;
+	String last_Name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getFirst_Name() {
+		return first_Name;
+	}
+	public void setFirst_Name(String first_Name) {
+		this.first_Name = first_Name;
+	}
+	public String getLast_Name() {
+		return last_Name;
+	}
+	public void setLast_Name(String last_Name) {
+		this.last_Name = last_Name;
+	}
+	
+
+}
---
+++ src/test/java/api/routes/Routes.java
@@ -0,0 +1,8 @@
+package api.routes;
+
+public class Routes {
+	
+	
+	public static String get_User_Call="https://www.test/v2";
+
+}
